ff7d37fc56607e8507a7d247f33fc94c8f391fe5
Amanda Foun <afoun@wellesley.edu>
2015-03-28 17:31:43 -0400

    changes the background

--- change.js
+++ change.js
@@ -7,13 +7,16 @@ var colors = {
   excited: {dark: "#ff8c00", light: "#ffdb4d"},
   calm: {dark: "#000d1a", light: "#0059b3"},
   meh:{dark: "#1a001a", light: "#b300b3"},
+  "default":{dark: "#1a001a", light: "#b300b3"}
 }
 
 /*gradient stuff*/
-window.onload = resetGradient("meh");
-window.onresize = resetGradient("meh");
+window.onload = resetGradient;
+window.onresize = resetGradient;
 
 function resetGradient(mood) {
+  console.log("resetting");
+  var body = document.querySelector("body");
   var gradient = document.querySelector("#gradient");
   var height = gradient.clientHeight;
   console.log(gradient);
@@ -22,25 +25,34 @@ function resetGradient(mood) {
   while (gradient.firstChild) {
     gradient.removeChild(gradient.firstChild);
   }
-  
-  gradient.style.backgroundColor = colors[mood].light;
+
+  if (colors[mood]) {
+    gradient.style.backgroundColor = colors[mood].light;
+    body.style.backgroundColor = colors[mood].light;
+  } 
+  else {
+    gradient.style.backgroundColor = colors["default"].light;
+    body.style.backgroundColor = colors["default"].light;
+  } 
   
   for (var i = height; i >= 0; i-=2) {
     var ombre = document.createElement("div");
     ombre.className = "ombre";
-    ombre.style.backgroundColor = colors[mood].dark;
+    if (colors[mood]) {ombre.style.backgroundColor = colors[mood].dark;} 
+    else {ombre.style.backgroundColor = colors["default"].dark;} 
     ombre.style.height = "2px";
     ombre.style.opacity = i/height;
     gradient.appendChild(ombre);
   }
 }
 
+
 // submit button
 var button = document.querySelector("button"); 
 
 button.onclick = function () {
   
-  var input = document.querySelector("#submit");
+  var input = document.querySelector("#thoughtbox");
   var value = input.value;
 
   if (value.search(/#excited/i) != -1 ) { //contains #excited
@@ -63,3 +75,4 @@ button.onclick = function () {
 function enter(e) {
   if (e.keyCode == 13) {button.click();}
 }
+
