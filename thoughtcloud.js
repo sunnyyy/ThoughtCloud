18262d3cf314472e0ccc7a2aef29d2b97195fdec
Priscilla Lee <plee3@wellesley.edu>
2015-03-28 17:26:50 -0400

    storing thoughts in object of objects

--- thoughtcloud.js
+++ thoughtcloud.js
@@ -1,24 +1,37 @@
-var thought_array = ["So many stickers at Brandeis #Codestellation #happy"];
+var thoughts = {happy:[], sad:[], angry:[], stressed:[], excited:[], calm:[], meh:[], other:[]};
 
+//"So many stickers at Brandeis #Codestellation #happy"];
 function submitting() {
-  successful_post();
   var thought_text = document.getElementById('thoughtbox').value;
   document.getElementById('HERE').innerHTML = thought_text;
-  thought_array.push(thought_text);
-}); // end of .click call
-
-function searching(string) {
-	var index;
-	var text;
-	for	(index = 0; index < thought_array.length; index++) {
-		if (thought_array[index].indexOf(string) > -1) {
-			//string is in this index of the array
-			text += "<div class='item'><p>" + thought_array[index] + "</p></div>";
-		}
+  var mood = getHashTag(thought_text);
+  if (thoughts[mood]) {
+	thoughts[mood].push(thought_text);
+  } else {
+	thoughts["other"].push(thought_text);
   }
-  document.getElementById('LIST').innerHTML = text;
 }
 
-successful_post() {
-	$(html).find('h1').css('color','red');
+function getHashTag(string) {
+	var mood = string.substring(string.indexOf("#")+1);
+	console.log(mood);
+	return mood;
+}
+
+function searching(string) {
+	var mood = string.substring(1);
+	if (thoughts[mood]) {
+		console.log(thoughts[mood]);
+	} else {
+		console.log("not a valid hashtag");
+	}
+	// var index;
+	// var text;
+	// for	(index = 0; index < thought_array.length; index++) {
+		// if (thought_array[index].indexOf(string) > -1) {
+			// //string is in this index of the array
+			// text += "<div class='item'><p>" + thought_array[index] + "</p></div>";
+		// }
+  // }
+  // document.getElementById('LIST').innerHTML = text;
 }
