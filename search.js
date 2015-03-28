c11eadae5807fb7fb0bc930bdfb433b5e542e7c3
Priscilla Lee <plee3@wellesley.edu>
2015-03-28 13:23:25 -0400

    added colors

--- search.js
+++ search.js
@@ -1,8 +1,19 @@
-/*gradient stuff*/
-window.onload = resetGradient;
-window.onresize = resetGradient;
+/*colors*/
+var colors = {
+	happy: {dark: "#001a0d", light: "#00b359"},
+	sad: {dark: "#333", light: "#999"},
+	angry: {dark: "#4d0000", light: "#e60000"},
+	stressed: {dark: "#b34700", light: "#ff944d"},
+	excited: {dark: "#ff8c00", light: "#ffdb4d"},
+	calm: {dark: "#000d1a", light: "#0059b3"},
+	meh:{dark: "#1a001a", light: "#b300b3"},
+}
 
-function resetGradient() {
+/*gradient stuff*/
+window.onload = resetGradient("meh");
+window.onresize = resetGradient("meh");
+
+function resetGradient(mood) {
 	var gradient = document.querySelector("#gradient");
 	var height = gradient.clientHeight;
 	console.log(gradient);
@@ -12,9 +23,12 @@ function resetGradient() {
 		gradient.removeChild(gradient.firstChild);
 	}
 	
+	gradient.style.backgroundColor = colors[mood].light;
+	
 	for (var i = height; i >= 0; i-=2) {
 		var ombre = document.createElement("div");
 		ombre.className = "ombre";
+		ombre.style.backgroundColor = colors[mood].dark;
 		ombre.style.height = "2px";
 		ombre.style.opacity = i/height;
 		gradient.appendChild(ombre);
@@ -33,8 +47,14 @@ button.onclick = function () {
 	var input = document.querySelector("#search");
 	var value = input.value;
 	
+	resetGradient(value);
+	
 	var result = document.createElement("div");
 	result.innerHTML = value;
 	result.className = "oneResult";
 	results.appendChild(result);
+}
+
+function enter(e) {
+	if (e.keyCode == 13) {button.click();}
 }
