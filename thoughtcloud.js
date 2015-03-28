81ed95395ab4d331d5f7e195164d369edc0f6a39
Priscilla Lee <plee3@wellesley.edu>
2015-03-28 18:36:27 -0400

    thought cloud stufff

--- thoughtcloud.js
+++ thoughtcloud.js
@@ -1,27 +1,32 @@
 var thoughts = {happy:[], sad:[], angry:[], stressed:[], excited:[], calm:[], meh:[], other:[]};
 
-//no hash tag at all: don't even save....but unpreselected hashtag save
+var submit_button = document.querySelector("#submit_button");
+submit_button.onclick = submitting;
 
-//"So many stickers at Brandeis #Codestellation #happy"];
+var search_button = document.querySelector("#search_button");
+search_button.onclick = searching;
+
 function submitting() {
-  var thought_text = document.getElementById('thoughtbox').value;
-  var moods = getHashTags(thought_text);
-  for (var m in moods) {
-	  if (thoughts[moods[m]]) {
-		thoughts[moods[m]].push(thought_text);
-	  } else {
-		thoughts["other"].push(thought_text);
-	  }
-  }
-	setTimeout(function(){  
-		$("#thoughtbox").fadeOut(); 
-		$("#submit_button").fadeOut();
-	}, 1000);
-	
-	setTimeout(function(){  
-		$("#thoughtbox").fadeIn(); 
-		$("#submit_button").fadeIn();
-	}, 1000);
+	var thought_text = document.getElementById('thoughtbox').value;
+	console.log("Submitted a thought: " + thought_text); 
+	var moods = getHashTags(thought_text);
+	for (var m in moods) {
+		if (thoughts[moods[m]]) {
+			thoughts[moods[m]].push(thought_text);
+		} else {
+			thoughts["other"].push(thought_text);
+		}
+	}
+
+	// setTimeout(function(){  
+		// $("#thoughtbox").fadeOut(); 
+		// $("#submit_button").fadeOut();
+	// }, 500);
+
+	// setTimeout(function(){  
+		// $("#thoughtbox").fadeIn(); 
+		// $("#submit_button").fadeIn();
+	// }, 1000);
 }
 
 function getHashTags(string) {
@@ -33,20 +38,39 @@ function getHashTags(string) {
 	return moods;
 }
 
-function searching(string) {
-	var mood = string.substring(1);
-	if (thoughts[mood]) {
-		console.log(thoughts[mood]);
-	} else {
-		console.log("not a valid hashtag");
+function searching() {
+	var search_text = document.getElementById('searchbox').value;
+	console.log("Searched a mood: " + search_text); 
+	var moods = getHashTags(search_text);
+	for (var m in moods) {
+		if (thoughts[moods[m]]) {
+			showResults(thoughts[moods[m]]);
+			console.log(thoughts[moods[m]]);
+		} else {
+			console.log(moods[m] + " isn't a valid value");
+		}
 	}
-	// var index;
-	// var text;
-	// for	(index = 0; index < thought_array.length; index++) {
-		// if (thought_array[index].indexOf(string) > -1) {
-			// //string is in this index of the array
-			// text += "<div class='item'><p>" + thought_array[index] + "</p></div>";
-		// }
-  // }
-  // document.getElementById('LIST').innerHTML = text;
+}
+
+function showResults(array) {
+	$("#results").empty();
+	console.log("showing results");
+	for (var i in array) {
+		var oneRow = document.createElement("div");
+		
+		var circle1 = document.createElement("div");
+		circle1.className = "circle1";
+		var circle2 = document.createElement("div");
+		circle2.className = "circle2";
+		
+		var oneResult = document.createElement("div");
+		oneResult.className = "result";
+		oneResult.innerHTML = array[i];
+		
+		oneRow.appendChild(oneResult);
+		oneRow.appendChild(circle1);
+		oneRow.appendChild(circle2);
+		
+		$("#results").append(oneRow);
+	}
 }
