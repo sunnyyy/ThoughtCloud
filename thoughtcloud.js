0fe1aef15e4f9827eaf5b77ac45a7ad17f9ba8d6
sunnyyy <coconutcandy12@gmail.com>
2015-03-28 17:03:05 -0400

    more edits

--- thoughtcloud.js
+++ thoughtcloud.js
@@ -1,11 +1,23 @@
-var counter;
-var thoughts = ["So many stickers at Brandeis #Codestellation #happy"];
+var thought_array = ["So many stickers at Brandeis #Codestellation #happy"];
 
 function submitting() {
-  counter++;
   successful_post();
-  document.getElementById('HERE').innerHTML = document.getElementById('thoughtbox').value;
+  var thought_text = document.getElementById('thoughtbox').value;
+  document.getElementById('HERE').innerHTML = thought_text;
+  thought_array.push(thought_text);
 }); // end of .click call
+
+function searching(string) {
+	var index;
+	var text;
+	for	(index = 0; index < thought_array.length; index++) {
+		if (thought_array[index].indexOf(string) > -1) {
+			//string is in this index of the array
+			text += "<div class='item'><p>" + thought_array[index] + "</p></div>";
+		}
+  }
+  document.getElementById('LIST').innerHTML = text;
+}
 
 successful_post() {
 	$(html).find('h1').css('color','red');
